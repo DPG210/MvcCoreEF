557947d8224b33d9bad8439160670f20e42a7e5a
DPG210 <diegogrego210@gmail.com>
2026-02-09 14:23:14 +0100

    Primer dia de martes

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext:DbContext
+    {
+        //EL CONSTRUCTOR RECIBIRA SIEMPRE LAS OPCIONES PARA ESTE CONTEXTO
+        //LA CLASE QUE RECIBE ES DbContextOptions<Context>
+        //ESTAS option DEBEMOS ENVIARLAS A LA CLASE SUPER/BASE DEL DbContext
+        public HospitalContext(DbContextOptions<HospitalContext> options):base(options)
+        {}
+        //DEBEMOS TENE UNA COLECCION POR CADA MODEL
+        //DICHA COLECCION DEBE SER DE TIPO DbSet<T>
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
